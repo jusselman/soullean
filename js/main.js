8407df4518b5b26d1c59efc156ff759fb30de5a5
jusselman <usselmanjosh@gmail.com>
2018-06-10 09:55:45 -0700

    soullean

---
+++ js/main.js
@@ -0,0 +1,312 @@
+
+
+// VUE ROUTER FOR MENU SECTION //
+const one = {
+    template: `<div class="text-center">
+                <div class="container-fluid">
+                  <div class="row">
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Hummus Platters</h2>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                  </div>
+
+                  <div class="col-4">
+                    <img class="img-fluid" src='img/popular1.jpg' alt="bananarice">
+                  </div>
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Falafel Dishes</h2>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    </div>
+
+                  </div>
+                </div>
+               </div>`
+}
+const two = {
+    template: `<div class="text-center">
+                <div class="container-fluid">
+                  <div class="row">
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Hummus Platters</h2>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                  </div>
+
+                  <div class="col-4">
+                    <img class="img-fluid" src='img/popular2.jpg' alt="bananarice">
+                  </div>
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Falafel Dishes</h2>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    </div>
+
+                  </div>
+                </div>
+               </div>`
+}
+const three = {
+    template: `<div class="text-center">
+                <div class="container-fluid">
+                  <div class="row">
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Hummus Platters</h2>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                  </div>
+
+                  <div class="col-4">
+                    <img class="img-fluid" src='img/popular3.png' alt="bananarice">
+                  </div>
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Falafel Dishes</h2>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    </div>
+
+                  </div>
+                </div>
+               </div>`
+}
+
+const four = {
+    template: `<div class="text-center">
+                <div class="container-fluid">
+                  <div class="row">
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Hummus Platters</h2>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                  </div>
+
+                  <div class="col-4">
+                    <img class="img-fluid" src='img/popular4.jpg' alt="bananarice">
+                  </div>
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Falafel Dishes</h2>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    </div>
+
+                  </div>
+                </div>
+               </div>`
+}
+
+const five = {
+    template: `<div class="text-center">
+                <div class="container-fluid">
+                  <div class="row">
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Hummus Platters</h2>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                  </div>
+
+                  <div class="col-4">
+                    <img class="img-fluid" src='img/popular5.jpg' alt="bananarice">
+                  </div>
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Falafel Dishes</h2>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    </div>
+
+                  </div>
+                </div>
+               </div>`
+}
+
+const six = {
+    template: `<div class="text-center">
+                <div class="container-fluid">
+                  <div class="row">
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Hummus Platters</h2>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                    <h2>Tasty Hummus Plate</h2>
+                    <p><i>Choose from the following: Spicy, Original, Mint</i></p>
+                    <p class="price">$15</p>
+                  </div>
+
+                  <div class="col-4">
+                    <img class="img-fluid" src='img/popular6.jpg' alt="bananarice">
+                  </div>
+
+                    <div class="col-4 pt-4">
+                    <h2 class="fancy">Falafel Dishes</h2>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    <h2>Red Lentil Falafel Salad</h2>
+                    <p><i>Zesty salad served with pieces of traditional falafel</i></p>
+                    <p class="price">$15</p>
+                    </div>
+
+                  </div>
+                </div>
+               </div>`
+}
+
+const router = new VueRouter ({
+    routes: [
+      {
+          path: '/one',
+          component: one
+      },
+      {
+        path: '/two',
+        component: two
+      },
+      {
+        path: '/three',
+        component: three
+      },
+      {
+        path: '/four',
+        component: four
+      },
+      {
+        path: '/five',
+        component: five
+      },
+      {
+        path: '/six',
+        component: six
+      }
+    ]
+})
+
+var routeTest = new Vue({
+    router,
+    el: '#app',
+    data: {
+
+    },
+    methods: {
+
+    }
+}).$mount('#app');
+
+
+
+
+// CHANGE NAVBAR WHEN SCROLLED DOWN TO NEW SECTIONS //
+var home = $('#home').offset().top;
+var about = $('#about').offset().top;
+var menu = $('#menu').offset().top;
+var events = $('#events').offset().top;
+var contact = $('#contact').offset().top;
+
+$(document).scroll(function() {
+  var scrollPos = $(document).scrollTop();
+  if (scrollPos >= home && scrollPos < about) {
+    $('#change').css('background-color', '#f00');
+  } else if (scrollPos >= about && scrollPos < menu) {
+    $('#change').css('background-color', '#0f0');
+  } else if (scrollPos >= menu && scrollPos < events) {
+    $('#change').css('background-color', '#00f');
+  } else if (scrollPos >= events && scrollPos < contact) {
+    $('#change').css('background-color', '#00f');
+  }
+});
+// SCROLL MAGIC CONTROLLER //
+
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: '.red-cube'
+})
+.setClassToggle('img', 'show')
+.addTo(controller);
